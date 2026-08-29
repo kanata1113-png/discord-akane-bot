56e5559055901d3074d7fe591dccfaead73b25d7
kanata1113-png <kanata1113@gmail.com>
2026-08-29 17:47:24 +0900

    v35 phase4: expose repositories package

---
+++ repositories/__init__.py
@@ -0,0 +1,15 @@
+from repositories.guild_repository import GuildRepository
+from repositories.memory_repository import MemoryRepository
+from repositories.ranking_repository import RankingRepository
+from repositories.registry import RepositoryRegistry
+from repositories.ticket_repository import TicketRepository
+from repositories.user_repository import UserRepository
+
+__all__ = [
+    "GuildRepository",
+    "MemoryRepository",
+    "RankingRepository",
+    "RepositoryRegistry",
+    "TicketRepository",
+    "UserRepository",
+]
